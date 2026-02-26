5907403e37275feefe2acc5ef940405e2c68e725
Perrine Villers <26814178+Floopidou@users.noreply.github.com>
2026-02-26 16:18:06 +0100

    completed without optionals yet

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params["list_id"])
+    @bookmark = Bookmark.new
+    # @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(bookmark_params[:movie_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :lists, only: %i[index new show create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
+
+  root "lists#index"
 end
